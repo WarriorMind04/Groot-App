21296d3dbac44e4178d5fe7bc860ea667b9ab1d4
WarriorMind04 <88904471+WarriorMind04@users.noreply.github.com>
2025-12-02 10:34:31 +0100

    Xcode project

---
+++ GrootApp/GrootApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrootApp
+//
+//  Created by José Miguel Guerrero Jiménez on 02/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
