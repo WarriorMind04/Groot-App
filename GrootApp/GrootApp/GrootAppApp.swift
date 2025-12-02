c7c1dcf526546d6da66aa49e8ca7d2702bd506be
WarriorMind04 <88904471+WarriorMind04@users.noreply.github.com>
2025-12-02 12:21:18 +0100

    First real commit

---
+++ GrootApp/GrootApp/GrootAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrootAppApp.swift
+//  GrootApp
+//
+//  Created by José Miguel Guerrero Jiménez on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GrootAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
